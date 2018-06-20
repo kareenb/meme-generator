c6b89a4819d1025f7862deb80daeba92389ed622
kareenb <kareenb89@gmail.com>
2018-06-20 17:10:15 +0300

    changed img url in gImgs, func updateMemeInfo

--- June20 - 2030/meme-gen/js/main.js
+++ June20 - 2030/meme-gen/js/main.js
@@ -9,23 +9,23 @@ function renderImgs() {
     var elGallery = document.querySelector('.meme-imgs');
 
     gImgs.forEach(function (img) {
-        strHTML += `<img class="img img-${img.id}" src="${img.url}" onclick="renderImgOnCanvas(this, ${img.id})" />`;
+        strHTML += `<img class="img img-${img.id}" src="${img.url}" onclick="renderImgOnCanvas(${img.id})" />`;
     });
 
     elGallery.innerHTML = strHTML;
 }
 
 
-function renderImgOnCanvas(img, imgId) {
+function renderImgOnCanvas(imgId) {
+    updateMemeInfo(imgId);
+
     var elCanvas = document.querySelector('canvas');
     var ctx = elCanvas.getContext("2d");
+    var elSelectedImg = document.querySelector(`.img-${imgId}`);
     ctx.clearRect(0, 0, elCanvas.width, elCanvas.height);
     elCanvas.width = 400;
     elCanvas.height = 400;
-    ctx.drawImage(img, 0, 0, 400, 400);
-
-    gMeme.selectedImgId = imgId; // TODO: Delete?
-
+    ctx.drawImage(elSelectedImg, 0, 0, 400, 400);
 }
 
 
--- June20 - 2030/meme-gen/js/meme-service.js
+++ June20 - 2030/meme-gen/js/meme-service.js
@@ -4,36 +4,36 @@ console.log('meme generator');
 
 var gId = 0;
 var gImgs = [
-    { id: ++gId, url: `../meme-imgs/${gId}.jpg`, keywords: ['happy'] },
-    { id: ++gId, url: `../meme-imgs/${gId}.jpg`, keywords: ['happy'] },
-    { id: ++gId, url: `../meme-imgs/${gId}.jpg`, keywords: ['happy'] },
-    { id: ++gId, url: `../meme-imgs/${gId}.jpg`, keywords: ['happy'] },
-    { id: ++gId, url: `../meme-imgs/${gId}.jpg`, keywords: ['happy'] },
-    { id: ++gId, url: `../meme-imgs/${gId}.jpg`, keywords: ['happy'] },
-    { id: ++gId, url: `../meme-imgs/${gId}.jpg`, keywords: ['happy'] },
-    { id: ++gId, url: `../meme-imgs/${gId}.jpg`, keywords: ['happy'] },
-    { id: ++gId, url: `../meme-imgs/${gId}.jpg`, keywords: ['happy'] },
-    { id: ++gId, url: `../meme-imgs/${gId}.jpg`, keywords: ['happy'] },
-    { id: ++gId, url: `../meme-imgs/${gId}.jpg`, keywords: ['happy'] },
-    { id: ++gId, url: `../meme-imgs/${gId}.jpg`, keywords: ['happy'] },
-    { id: ++gId, url: `../meme-imgs/${gId}.jpg`, keywords: ['happy'] },
-    { id: ++gId, url: `../meme-imgs/${gId}.jpg`, keywords: ['happy'] },
-    { id: ++gId, url: `../meme-imgs/${gId}.jpg`, keywords: ['happy'] },
-    { id: ++gId, url: `../meme-imgs/${gId}.jpg`, keywords: ['happy'] },
-    { id: ++gId, url: `../meme-imgs/${gId}.jpg`, keywords: ['happy'] },
-    { id: ++gId, url: `../meme-imgs/${gId}.jpg`, keywords: ['happy'] },
-    { id: ++gId, url: `../meme-imgs/${gId}.jpg`, keywords: ['happy'] },
-    { id: ++gId, url: `../meme-imgs/${gId}.jpg`, keywords: ['happy'] },
-    { id: ++gId, url: `../meme-imgs/${gId}.jpg`, keywords: ['happy'] },
-    { id: ++gId, url: `../meme-imgs/${gId}.jpg`, keywords: ['happy'] },
-    { id: ++gId, url: `../meme-imgs/${gId}.jpg`, keywords: ['happy'] },
-    { id: ++gId, url: `../meme-imgs/${gId}.jpg`, keywords: ['happy'] },
-    { id: ++gId, url: `../meme-imgs/${gId}.jpg`, keywords: ['happy'] }
+    { id: ++gId, url: `../meme-gen/meme-imgs/${gId}.jpg`, keywords: ['happy'] },
+    { id: ++gId, url: `../meme-gen/meme-imgs/${gId}.jpg`, keywords: ['happy'] },
+    { id: ++gId, url: `../meme-gen/meme-imgs/${gId}.jpg`, keywords: ['happy'] },
+    { id: ++gId, url: `../meme-gen/meme-imgs/${gId}.jpg`, keywords: ['happy'] },
+    { id: ++gId, url: `../meme-gen/meme-imgs/${gId}.jpg`, keywords: ['happy'] },
+    { id: ++gId, url: `../meme-gen/meme-imgs/${gId}.jpg`, keywords: ['happy'] },
+    { id: ++gId, url: `../meme-gen/meme-imgs/${gId}.jpg`, keywords: ['happy'] },
+    { id: ++gId, url: `../meme-gen/meme-imgs/${gId}.jpg`, keywords: ['happy'] },
+    { id: ++gId, url: `../meme-gen/meme-imgs/${gId}.jpg`, keywords: ['happy'] },
+    { id: ++gId, url: `../meme-gen/meme-imgs/${gId}.jpg`, keywords: ['happy'] },
+    { id: ++gId, url: `../meme-gen/meme-imgs/${gId}.jpg`, keywords: ['happy'] },
+    { id: ++gId, url: `../meme-gen/meme-imgs/${gId}.jpg`, keywords: ['happy'] },
+    { id: ++gId, url: `../meme-gen/meme-imgs/${gId}.jpg`, keywords: ['happy'] },
+    { id: ++gId, url: `../meme-gen/meme-imgs/${gId}.jpg`, keywords: ['happy'] },
+    { id: ++gId, url: `../meme-gen/meme-imgs/${gId}.jpg`, keywords: ['happy'] },
+    { id: ++gId, url: `../meme-gen/meme-imgs/${gId}.jpg`, keywords: ['happy'] },
+    { id: ++gId, url: `../meme-gen/meme-imgs/${gId}.jpg`, keywords: ['happy'] },
+    { id: ++gId, url: `../meme-gen/meme-imgs/${gId}.jpg`, keywords: ['happy'] },
+    { id: ++gId, url: `../meme-gen/meme-imgs/${gId}.jpg`, keywords: ['happy'] },
+    { id: ++gId, url: `../meme-gen/meme-imgs/${gId}.jpg`, keywords: ['happy'] },
+    { id: ++gId, url: `../meme-gen/meme-imgs/${gId}.jpg`, keywords: ['happy'] },
+    { id: ++gId, url: `../meme-gen/meme-imgs/${gId}.jpg`, keywords: ['happy'] },
+    { id: ++gId, url: `../meme-gen/meme-imgs/${gId}.jpg`, keywords: ['happy'] },
+    { id: ++gId, url: `../meme-gen/meme-imgs/${gId}.jpg`, keywords: ['happy'] },
+    { id: ++gId, url: `../meme-gen/meme-imgs/${gId}.jpg`, keywords: ['happy'] }
 ];
 
 
 var gMeme = {
-                selectedImgId: 5, // TODO: Delete?
+                selectedImgId: 5,
                 txts: [
                         { line: 'I never eat Falafel', size: 20, align: 'left', color: 'red' }
                     ] 
@@ -41,4 +41,8 @@ var gMeme = {
 
 function updateTxt(txt) {
     gMeme.txts[0].line = txt;
+}
+
+function updateMemeInfo(imgId) {
+    gMeme.selectedImgId = imgId;
 }
