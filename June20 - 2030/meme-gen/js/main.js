28add38fdf2346c18b8bca7be2b6be4fba59c9dc
Shlomi <Shlomixg@gmail.com>
2018-06-20 19:34:17 +0300

    func toggleSectios()

--- June20 - 2030/meme-gen/js/main.js
+++ June20 - 2030/meme-gen/js/main.js
@@ -4,12 +4,17 @@ console.log('main');
 
 renderImgs();
 
+function initPage() {
+    var elGallery = document.querySelector('.meme-imgs');
+    elGallery.style.maxHeight = elGallery.scrollHeight + "px";
+}
+
 function renderImgs() {
     var strHTML = '';
     var elGallery = document.querySelector('.meme-imgs');
 
     gImgs.forEach(function (img) {
-        strHTML += `<img class="img img-${img.id}" src="${img.url}" onclick="renderImgOnCanvas(${img.id})" />`;
+        strHTML += `<img class="img img-${img.id}" src="${img.url}" onclick="renderImgOnCanvas(${img.id}); toggleSections()" />`;
     });
 
     elGallery.innerHTML = strHTML;
@@ -30,7 +35,7 @@ function renderImgIntoCanvas(imgId) {
     var elCanvas = document.querySelector('canvas');
     var ctx = elCanvas.getContext("2d");
     ctx.clearRect(0, 0, elCanvas.width, elCanvas.height);
-    
+
     var elSelectedImg = document.querySelector(`.img-${imgId}`);
     var image = new Image(0, 0);
     image.src = elSelectedImg.src;
@@ -40,11 +45,7 @@ function renderImgIntoCanvas(imgId) {
     ctx.drawImage(image, 0, 0, elCanvas.width, elCanvas.height);
 }
 
-<<<<<<< HEAD
 function readUserLine() {
-=======
-function renderTxtOnCanvas() {
->>>>>>> a36cb8b59de15666992261e5b2699aba47954678
     var elTxtBox = document.querySelector('.user-txt-box');
     updateTxt(elTxtBox.value);
     renderTxtOnCanvas();
@@ -57,4 +58,29 @@ function renderTxtOnCanvas() {
     ctx.font = "40px 'Arial'";
     ctx.fillStyle = 'black';
     ctx.fillText(gMeme.txts[0].line, 20, 20);
+}
+
+function toggleSections() {
+    console.log('jkj');
+    
+    toggleMemeEditor();
+    toggleGallery();
+}
+
+function toggleMemeEditor() {
+    var elMemeEditor = document.querySelector('.meme-editor');
+    if (elMemeEditor.style.maxHeight) {
+        elMemeEditor.style.maxHeight = null;
+    } else {
+        elMemeEditor.style.maxHeight = elMemeEditor.scrollHeight + "px";
+    }
+}
+
+function toggleGallery() {
+    var elGallery = document.querySelector('.meme-imgs');
+    if (elGallery.style.maxHeight) {
+        elGallery.style.maxHeight = null;
+    } else {
+        elGallery.style.maxHeight = elGallery.scrollHeight + "px";
+    }
 }
