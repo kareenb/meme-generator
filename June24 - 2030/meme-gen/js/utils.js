d3c39c3103611ccf6cafedb45bafd583a96b0d1d
Shlomi <Shlomixg@gmail.com>
2018-06-22 14:33:30 +0300

    adding fontawesome, improve contact-us section

---
+++ June24 - 2030/meme-gen/js/utils.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function saveToStorage(key, value) {
+    localStorage.setItem(key, JSON.stringify(value))
+}
+
+function loadFromStorage(key) {
+    return JSON.parse(localStorage.getItem(key))
+}
+
+function removeFromStorage(key) {
+    localStorage.removeItem(key);
+}
